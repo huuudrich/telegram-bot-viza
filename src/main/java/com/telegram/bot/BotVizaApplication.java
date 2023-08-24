0de741f727272479acbb92f80e6ed101883b600c
huuudrich <108597096+huuudrich@users.noreply.github.com>
2023-08-24 20:59:19 +1000

    spring init

---
+++ src/main/java/com/telegram/bot/BotVizaApplication.java
@@ -0,0 +1,13 @@
+package com.telegram.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotVizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotVizaApplication.class, args);
+	}
+
+}
