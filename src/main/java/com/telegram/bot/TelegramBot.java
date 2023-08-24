b5da2276f58af5e42621b331dc40b225ee8c8232
huuudrich <108597096+huuudrich@users.noreply.github.com>
2023-08-24 21:22:32 +1000

    tg bot init

---
+++ src/main/java/com/telegram/bot/TelegramBot.java
@@ -0,0 +1,59 @@
+package com.telegram.bot;
+
+import com.telegram.bot.config.BotConfig;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@AllArgsConstructor
+public class TelegramBot extends TelegramLongPollingBot {
+    private final BotConfig botConfig;
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "":
+                default:
+            }
+        }
+
+    }
+
+    private void startCommandReceived(Long chatId, String name) {
+        String answer = "ЗДАРОВА ХУЕСОСИК ЧЕ РЕШИЛ ГРАБАНУТЬ КИТАЙ ????";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(Long chatId, String textToSend) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+}
